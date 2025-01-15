4266866f7db05c1e4232b0c449b6ecadbd95df97
nicolaalex <157640239+nicolaalex@users.noreply.github.com>
2025-01-15 16:24:01 -0600

    Update and rename Greeter.java to Main.java

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Hello, World!");
+        System.out.println("How old are you?");
+        int age = 0;
+        do {
+            try {
+                age = scan.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a number");
+                System.out.println(e.getMessage());
+                scan.next();
+            }
+        }while ((age != -2));
+
+        System.out.println("You are " + age + " years old");
+    }
+
+
+
+}
